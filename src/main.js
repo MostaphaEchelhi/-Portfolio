703f3235305bd1494e2b5ebbf54b67b344ed884e
MostaphaEchelhi <mostapha.echelhi@medieinstitutet.se>
2023-02-21 17:03:36 +0100

    UPDATE

--- src/main.js
+++ src/main.js
@@ -1,10 +1,37 @@
 const navMenu = document.querySelector("nav");
 const menuBtn = document.querySelector(".btnMenu")
+const toggleBg = document.querySelector(".toSpace")
+
+let orginalColor = true;
+
 
 menuBtn.addEventListener("click", openAndCloseMenu);
+toggleBg.addEventListener("click", switchBackground, textToBtn);
+
+
 
 
 function openAndCloseMenu () {
     navMenu.classList.toggle("open");
 } 
 
+function switchBackground() {
+    orginalColor = !orginalColor;
+    
+    if (orginalColor) {
+        document.body.classList.remove("dark");
+        document.body.classList.add("light");
+    } else {
+        document.body.classList.remove("light");
+        document.body.classList.add("dark");
+    }
+
+}
+
+function textToBtn() {
+    if (textToBtn) {
+        document.body.innerText = "TO SPACE";
+    } else
+
+        document.body.innerHTML = "TO ERTHE"
+}
