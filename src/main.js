5ff34e254cd2b5647846d7fae5041bf990c55770
MostaphaEchelhi <mostapha.echelhi@medieinstitutet.se>
2023-03-26 20:59:19 +0200

    hotfix

--- src/main.js
+++ src/main.js
@@ -14,24 +14,3 @@ toggleBg.addEventListener("click", switchBackground, textToBtn);
 function openAndCloseMenu () {
     navMenu.classList.toggle("open");
 } 
-
-function switchBackground() {
-    orginalColor = !orginalColor;
-    
-    if (orginalColor) {
-        document.body.classList.remove("dark");
-        document.body.classList.add("light");
-    } else {
-        document.body.classList.remove("light");
-        document.body.classList.add("dark");
-    }
-
-}
-
-function textToBtn() {
-    if (textToBtn) {
-        document.body.innerText = "TO SPACE";
-    } else
-
-        document.body.innerHTML = "TO ERTHE"
-}
